a683687847f4188c5b077b27c0696415da621ec8
pliamoconnor <40121780+pliamoconnor@users.noreply.github.com>
2018-07-16 11:48:13 -0400

    new stuff

---
+++ TortureNotesPrototype/TotureNotePrototype/Assets/Script/ObjectSelector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSelector : MonoBehaviour
+{
+    public event OnCollisionEvent collisionCallback;
+    public delegate void OnCollisionEvent();
+
+    void Start()
+    {
+        collisionCallback += UIManager.instance.OnObjectSelected;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {        
+        if(other.tag.ToLower() == "selectable")
+        {
+            collisionCallback();
+        }
+    }
+}
---
+++ TortureNotesPrototype/TotureNotePrototype/Assets/Script/UIManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+    public Canvas AnnotationWindow;
+
+    void Start ()
+    {
+		if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void OnObjectSelected()
+    {
+       
+    }
+}

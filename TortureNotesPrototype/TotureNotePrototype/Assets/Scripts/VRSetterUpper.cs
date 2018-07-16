09df2cf64047f414dfa920d33ff9958066aed559
APFgameDev <39070845+APFgameDev@users.noreply.github.com>
2018-07-16 12:37:50 -0400

    Added VR Container prefab and VRSetterUpper.cs

---
+++ TortureNotesPrototype/TotureNotePrototype/Assets/Scripts/VRSetterUpper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+struct InputAxis
+{
+    public const string LeftIndexTrigger = "LeftIndexTrigger";
+    public const string RightIndexTrigger = "RightIndexTrigger";
+    public const string LeftGripTrigger = "LeftGripTrigger";
+    public const string RightGripTrigger = "RightGripTrigger";
+}
+
+
+public class VRSetterUpper : MonoBehaviour {
+    [SerializeField]
+    Transform leftHand;
+    [SerializeField]
+    Transform rightHand;
+    [SerializeField]
+    Vector3 posOffSet = new Vector3(0,1,0);
+	
+	// Update is called once per frame
+	void Update () {
+        leftHand.localPosition = InputTracking.GetLocalPosition(XRNode.LeftHand) + posOffSet;
+        leftHand.localRotation = InputTracking.GetLocalRotation(XRNode.LeftHand);
+        rightHand.localPosition =  InputTracking.GetLocalPosition(XRNode.RightHand) + posOffSet;
+        rightHand.localRotation = InputTracking.GetLocalRotation(XRNode.RightHand);
+    }
+
+}

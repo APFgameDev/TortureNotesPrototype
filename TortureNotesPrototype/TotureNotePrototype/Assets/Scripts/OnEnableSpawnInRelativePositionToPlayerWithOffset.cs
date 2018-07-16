238bda9115c6ba520625deb0118fff58a1ffae23
APFgameDev <39070845+APFgameDev@users.noreply.github.com>
2018-07-16 16:24:33 -0400

    added OnEnableSpawnInRelativePositionToPlayerWithOffset.cs

---
+++ TortureNotesPrototype/TotureNotePrototype/Assets/Scripts/OnEnableSpawnInRelativePositionToPlayerWithOffset.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnEnableSpawnInRelativePositionToPlayerWithOffset : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 offset;
+
+    private void OnEnable()
+    {
+        transform.position = Camera.main.transform.position + Camera.main.transform.rotation * offset;
+    }
+}

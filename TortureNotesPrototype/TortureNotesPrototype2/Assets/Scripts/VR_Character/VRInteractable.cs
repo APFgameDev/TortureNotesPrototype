b81f6e7041e670199ba6d707294d5e80b47fc66e
pliamoconnor <40121780+pliamoconnor@users.noreply.github.com>
2018-07-24 11:01:08 -0400

    fuck

--- TortureNotesPrototype/TortureNotesPrototype2/Assets/Scripts/VR_Character/VRInteractable.cs
+++ TortureNotesPrototype/TortureNotesPrototype2/Assets/Scripts/VR_Character/VRInteractable.cs
@@ -1,28 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.Events;
 using UnityEngine;
 
 public class VRInteractable : MonoBehaviour
 {
+    [SerializeField]
+    private InteractEvents m_Events;
+
     public virtual void OnHoverEnter(VRInteractionData vrInteraction)
     {
-
+        m_Events.OnHoverEnter.Invoke();
     }
     public virtual void OnHoverExit(VRInteractionData vrInteraction)
     {
-
+        m_Events.OnHoverExit.Invoke();
     }
     public virtual void OnClick(VRInteractionData vrInteraction)
     {
-
+        m_Events.OnClick.Invoke();
     }
     public virtual void OnClickHeld(VRInteractionData vrInteraction)
     {
-
+        m_Events.OnClickHeld.Invoke();
     }
     public virtual void OnClickRelease(VRInteractionData vrInteraction)
     {
-
+        m_Events.OnClickRelease.Invoke();
     }
 }
 
@@ -33,4 +35,14 @@ public struct VRInteractionData
     public Vector2 movementDirection;
     public bool secondaryClickPressed;
     public System.Action<Color> changeColor;
+}
+
+[System.Serializable]
+public struct InteractEvents
+{
+    public UnityEvent OnHoverEnter;
+    public UnityEvent OnHoverExit;
+    public UnityEvent OnClick;
+    public UnityEvent OnClickHeld;
+    public UnityEvent OnClickRelease;
 }

e21b68993f4a24637850f54df489d9d28004147c
CodyTheCoderWhoCodes <41305486+CodyTheCoderWhoCodes@users.noreply.github.com>
2018-07-24 11:27:32 -0400

    Added text script and sharp text fix

---
+++ TortureNotesPrototype/TortureNotesPrototype2/Assets/Editor/Tools.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class Tools
+{
+    [MenuItem("GameObject/BetterUI/CreateText", false, 0)]
+    private static void GetHelp()
+    {
+        GameObject obj = new GameObject("Text");
+        Text text = obj.AddComponent<Text>();
+        Outline outline = obj.AddComponent<Outline>();
+        
+        GameObject parent = Selection.activeGameObject;
+
+        if(parent != null)
+        {
+            obj.transform.SetParent(parent.transform);
+            obj.transform.localPosition = Vector3.zero;
+        }
+
+        text.rectTransform.sizeDelta = new Vector2(160.0f, 30.0f);
+        text.text = "New Text Test";
+        text.color = Color.black;
+
+        Font ArialFont = (Font)Resources.Load("Fonts/Arial");
+        text.font = ArialFont;
+        text.material = ArialFont.material;
+
+        outline.effectColor = Color.white;
+        outline.effectDistance = new Vector2(0.1f, 0.1f);
+    }
+}

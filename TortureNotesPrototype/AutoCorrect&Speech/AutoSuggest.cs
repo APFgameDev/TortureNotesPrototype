64c0766aead2bf71ed2415845f3055d33ff20681
APFgameDev <39070845+APFgameDev@users.noreply.github.com>
2018-07-27 01:06:30 -0400

    Added AutoCorrect&Speech

---
+++ TortureNotesPrototype/AutoCorrect&Speech/AutoSuggest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoSuggest : MonoBehaviour
+{
+    /*//For Testing
+    
+    [SerializeField]
+    string query;
+    [SerializeField]
+    string[] suggestions;
+
+    private void Update()
+    {
+        QueryForSuggestions(query, (a) => { suggestions = a; });
+    }
+  
+   */
+
+    string prevQuery;
+
+    public void QueryForSuggestions(string query, Action<string[]> callBackWithSuggestions)
+    {
+        //no need to constantly ask database about same query
+        if (prevQuery == query)
+            return;
+
+        prevQuery = query;
+        StartCoroutine(GetWebSuggestion(query, callBackWithSuggestions));
+    }
+
+    IEnumerator GetWebSuggestion(string query, Action<string[]> callBackWithSuggestions)
+    {
+        if (query == string.Empty)
+            callBackWithSuggestions(new string[0]);
+        else
+        {
+            using (WWW www = new WWW("https://api.datamuse.com/sug?s=" + query))
+            {
+                yield return www;
+
+                string rawSuggestionQueryReturn = www.text;
+
+                rawSuggestionQueryReturn = rawSuggestionQueryReturn.Remove(0, 1);
+                rawSuggestionQueryReturn = rawSuggestionQueryReturn.Remove(rawSuggestionQueryReturn.Length - 1, 1);
+
+                string[] suggestions = rawSuggestionQueryReturn.Split(new string[] { "},{" }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 0; i < suggestions.Length; i++)
+                {
+                    string[] split = suggestions[i].Split(new string[] { "\":\"", "\",\"" }, StringSplitOptions.RemoveEmptyEntries);
+                    suggestions[i] = split[1];
+                }
+
+                callBackWithSuggestions(suggestions);
+            }
+        }
+    }
+}
---
+++ TortureNotesPrototype/AutoCorrect&Speech/TryRecord.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Windows.Speech;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System;
+
+[RequireComponent(typeof(AudioSource))]
+public class TryRecord : MonoBehaviour {
+
+    public int m_recordSeconds = 10;
+
+    AudioSource audioSource;
+    DictationRecognizer dictationRecognizer;
+    PhraseRecognizer phraseRecognizer;
+    string microphone;
+
+    [SerializeField]
+    UnityEngine.UI.Text RecordButtonText;
+    [SerializeField]
+    UnityEngine.UI.Text RecordText;
+
+    [SerializeField]
+
+    UnityEngine.UI.Text dictationText;
+
+    private void Start()
+    {
+        dictationRecognizer = new DictationRecognizer(ConfidenceLevel.Low);
+        dictationRecognizer.DictationResult += DictationRecognizer_DictationResult;
+
+        audioSource = GetComponent<AudioSource>();
+
+        foreach (string device in Microphone.devices)
+        {
+            microphone = device;
+        }
+
+        RecordText.text = "Record Audio";
+
+        RecordText.text = "";
+    }
+
+    private void DictationRecognizer_DictationResult(string text, ConfidenceLevel confidence)
+    {
+        dictationText.text = text;
+    }
+
+    public void RecordAudio()
+    {
+        if (!Microphone.IsRecording(microphone))
+        {
+            audioSource.clip = Microphone.Start(microphone, false, m_recordSeconds, 44100);
+            StartCoroutine(WaitForEndRecord());
+        }
+        else
+        {
+            Microphone.End(microphone);
+        }
+    }
+
+    IEnumerator WaitForEndRecord()
+    {
+        RecordButtonText.text = "Stop Record";
+        dictationRecognizer.Start();
+
+        while (Microphone.IsRecording(microphone))
+        {
+            yield return new WaitForEndOfFrame();
+
+            RecordText.text = "Recording Time Remaining = " + (m_recordSeconds - Microphone.GetPosition(microphone) / 44100.0f);
+
+
+        }
+
+        RecordButtonText.text = "Record Audio";
+    }
+
+    public void PlayRecordedAudio()
+    {
+        audioSource.Play();
+    }
+
+    private void OnDestroy()
+    {
+        dictationRecognizer.Dispose();
+    }
+
+    public void SaveClip()
+    {
+        SavWav.Save("Test", audioSource.clip);
+    }
+}

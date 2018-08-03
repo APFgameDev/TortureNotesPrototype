b4bc7f2a907724f099ff1bd0c772bdb1b947ee10
pliamoconnor <40121780+pliamoconnor@users.noreply.github.com>
2018-08-03 09:58:41 -0400

    Selection of SO values added

---
+++ Annotations V3/Assets/Scripts/SO Scripts/IntReference.cs
@@ -0,0 +1,37 @@
+﻿namespace NS_Annotation.NS_SO
+{
+    [System.Serializable]
+    public class IntReference
+    {
+        /// <summary>Whether to use an editor-assigned constant or a ScriptableObject variable</summary>
+        public bool UseVariable = false;
+        /// <summary>Constant value. Primarily used for testing and debuging</summary>
+        public int ConstantValue;
+        /// <summary>Linked ScriptableObject variable</summary>
+        public IntVariable Variable;
+
+        /// <summary>Both Get and Set affect ConstantValue or Variable.Value, depending on UseVariable</summary>
+        public int Value
+        {
+            get
+            {
+                if (!UseVariable || (!UnityEngine.Application.isPlaying && Variable == null))
+                    return ConstantValue;
+                else
+                    return Variable.Value;
+            }
+            set
+            {
+                if (!UseVariable || (!UnityEngine.Application.isPlaying && Variable == null))
+                    ConstantValue = value;
+                else
+                    Variable.Value = value;
+            }
+        }
+
+        public static implicit operator int(IntReference reference)
+        {
+            return reference.Value;
+        }
+    }
+}
---
+++ Annotations V3/Assets/Scripts/SO Scripts/VoidEvent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace NS_Annotation.NS_SO
+{
+    /// <summary>ScriptableObject containing an event that several listeners respond to when published</summary>
+    [CreateAssetMenu(fileName = "VoidEvent", menuName = "SO Events/Void", order = 0)]
+    public class VoidEvent : ScriptableObject
+    {
+        List<VoidEventListener> m_Listeners = new List<VoidEventListener>();
+
+        public void Publish()
+        {
+            for (int i = m_Listeners.Count - 1; i >= 0; i--)
+            {
+                m_Listeners[i].OnEventPublished();
+            }
+        }
+
+        /// <summary>Adds argument to list of listeners invoked when this GameEvent is published</summary>
+        /// <param name="a_ToSubscribe">Listener to invoke</param>
+        public void SubscribeListener(VoidEventListener a_ToSubscribe)
+        {
+            if (!m_Listeners.Contains(a_ToSubscribe))
+            {
+                m_Listeners.Add(a_ToSubscribe);
+            }
+        }
+
+        /// <summary>Removes argument listener from list so it no longer listens to event</summary>
+        /// <param name="a_ToUnSubscribe">Listener to remove</param>
+        public void UnSubscribeListener(VoidEventListener a_ToUnSubscribe)
+        {
+            if (m_Listeners.Contains(a_ToUnSubscribe))
+            {
+                m_Listeners.Remove(a_ToUnSubscribe);
+            }
+        }
+    }
+}

354b26b776cc8b905d872cb6b2877f3dc5c06d9a
pliamoconnor <40121780+pliamoconnor@users.noreply.github.com>
2018-08-03 10:56:58 -0400

    Added the core vr functionality

---
+++ Annotations V3/Assets/Scripts/VR User/VRUser.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.XR;
+using UnityEngine;
+
+public class VRUser : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_LeftHand;
+    [SerializeField]
+    private Transform m_RightHand;
+
+    void Update()
+    {
+        m_LeftHand.localPosition = InputTracking.GetLocalPosition(XRNode.LeftHand);
+        m_LeftHand.localRotation = InputTracking.GetLocalRotation(XRNode.LeftHand);
+
+        m_RightHand.localPosition = InputTracking.GetLocalPosition(XRNode.RightHand);
+        m_RightHand.localRotation = InputTracking.GetLocalRotation(XRNode.RightHand);
+    }
+}

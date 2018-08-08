576a35bf9d5684c23ec29bd8711416aea2401e63
pliamoconnor <40121780+pliamoconnor@users.noreply.github.com>
2018-08-08 09:09:52 -0400

    EH

---
+++ Annotations V3/Assets/Scripts/Utilities/ActiveToggle.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveToggle : MonoBehaviour
+{
+    public void SetActive()
+    {
+        gameObject.SetActive(!gameObject.activeInHierarchy);
+    }
+}
